75419a40a004e4bcc4f724c1c2a3748f0c26eafd
AdrienBlanco <adrienblanco89@gmail.com>
2023-05-11 16:34:33 +0200

    Diminution de l'indentation

--- assets/script.js
+++ assets/script.js
@@ -1,41 +1,26 @@
-const slides = [ //Slides content array
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+//Slides content array
+const slides = [ 
+	{ "image":"slide1.jpg", "tagLine":"Impressions tous formats <span>en boutique et en ligne</span>" },
+	{ "image":"slide2.jpg", "tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>" },
+	{ "image":"slide3.jpg", "tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>" },
+	{ "image":"slide4.png", "tagLine":"Autocollants <span>avec découpe laser sur mesure</span>" }
+];
 
 //Bullet points HTML creation function
 function dotCreation() {
-	//Bullet points container selector
-	let dots = document.querySelector('#banner .dots');
-	//Bullet points div creation
-	let dot = document.createElement('div');
-	//Bullet points class naming
-	dot.classList.add("dot");
-	//Bullet points container attachment
-	dots.appendChild(dot);
+	let dots = document.querySelector('#banner .dots'); //Bullet points container selector
+	let dot = document.createElement('div'); //Bullet points div creation
+	dot.classList.add("dot"); //Bullet points class naming
+	dots.appendChild(dot); //Bullet points container attachment
 };
 //Bullet points creation loop
 for(let i = 0; i < slides.length; i++){ //i start at 0 ; if i < slides.length ; then add +1 to i
     dotCreation();
 };
 
-//Variables used for arrows EventListener functions 
+// Variables used for carousel function
 
-//Index number one
+//Index number one 
 let current = 0;
 
 //Bullet points array creation
@@ -54,14 +39,13 @@ function carousel(index) {
 	dotList[current].classList.add('dot_selected'); //add the class 'dot_selected' to the current dot
 	image.src = "./assets/images/slideshow/" + slides[current].image; //replace image.src with the current table index and the key "image" 
 	tagLine.innerHTML = slides[current].tagLine; //replace <p> with the current table index and the key "tagLine"
-}
+};
 
 //Right arrow eventListener
 document.querySelector('#banner .arrow_right').addEventListener('click', function() {
 	if(current == slides.length - 1) { //condition: if current index equal slides.length -1, then >>
-		carousel(0) //index becomes 0
-	}
-	else { 
+		carousel(0) //index becomes 0 
+	} else { 
 		carousel(current + 1) //index becomes current index +1
 	};
 });
@@ -70,8 +54,7 @@ document.querySelector('#banner .arrow_right').addEventListener('click', functio
 document.querySelector('#banner .arrow_left').addEventListener('click', function() {
 	if(current == 0) { //condition: if current index equal 0, then >>
 		carousel(slides.length - 1) //index becomes slides.length -1
-	}
-	else { 
+	} else { 
 		carousel(current - 1) //index becomes current index -1
 	};
 });
